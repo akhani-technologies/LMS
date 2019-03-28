2e800c2fe6d7314b38f0f61b03bceb439d043bc2
neo <neo@zimeletechnologies.com>
2019-03-28 22:57:13 +0200

    connection

--- webapp/PHP/onSendImage.php
+++ webapp/PHP/onSendImage.php
@@ -1,13 +1,15 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-$ftp_server=" ftp://34.73.21.183:8021";
+// $ftp_server=" ftp://34.73.21.183:8021";
+$ftp_server = "34.73.21.183";
+$ftp_conn = ftp_ssl_connect($ftp_server) or die("Could not connect to $ftp_server");
 $ftp_user_name="akhani";
 $ftp_user_pass='@kh@n!';
 $file = "C:\\Users\NeoLe\Downloads\9001018980085.png"; 
 $remote_file = "C:\\images\fingerprint\9001018980085.png";
 
 // set up basic connection
-$conn_id = ftp_connect($ftp_server) or die("Could not connect to $ftp_server");;
+// $conn_id = ftp_connect($ftp_server) or die("Could not connect to $ftp_server");;
 
 // login with username and password
 $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
