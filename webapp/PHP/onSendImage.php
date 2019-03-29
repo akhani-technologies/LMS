b2d2e73eb3391dc460644835e5d51a46a25ae61c
neo <neo@zimeletechnologies.com>
2019-03-29 09:17:45 +0200

    test ftp

--- webapp/PHP/onSendImage.php
+++ webapp/PHP/onSendImage.php
@@ -1,28 +1,46 @@
 <?php
-header('Access-Control-Allow-Origin: *');
-// $ftp_server=" ftp://34.73.21.183:8021";
-$ftp_server = "34.73.21.183";
-$ftp_conn = ftp_ssl_connect($ftp_server) or die("Could not connect to $ftp_server");
-$ftp_user_name="akhani";
-$ftp_user_pass='@kh@n!';
-$file = "C:\Users\NeoLe\Downloads\9001018980085.png"; 
-$remote_file = "C:\\images\fingerprint\9001018980085.png";
+
+$ftp_server="34.73.21.183";
+$ftp_port= "21";
+$ftp_serusername="akhani";
+$ftp_serpass="@kh@n!";
+$destpath = "C:\images\fingerprints";
+$destination_file = "9001018980085.png";
+$source_file = "/Users/NeoLe/Downloads/9001018980085.png";
+
 
 // set up basic connection
-// $conn_id = ftp_connect($ftp_server) or die("Could not connect to $ftp_server");;
+$conn_id = ftp_connect($ftp_server, $ftp_port) or die("Couldn't connect to $ftp_server");
+echo "here 0";
 
 // login with username and password
-$login_result = ftp_login($ftp_conn, $ftp_user_name, $ftp_user_pass);
+//if($conn_id){
+$login_result = ftp_login($conn_id, $ftp_serusername, $ftp_serpass);
+//},
+echo "here 1";
 
-// upload a file
-if (ftp_put($conn_id, $remote_file, $file, FTP_ASCII)) {
-    echo "successfully uploaded $file\n";
+// check connection
+if ((!$conn_id) || (!$login_result)) {
+    echo "FTP connection has failed!";
     exit;
+}
+
+// upload the file
+ftp_chdir($conn_id, $destpath);
+$upload = ftp_put($conn_id, $destination_file, $source_file, FTP_BINARY);
+
+// check upload status
+if (!$upload) {
+    echo "FTP upload has failed!";
 } else {
-    echo "There was a problem while uploading $file\n";
-    exit;
-    }
-// close the connection
+    echo "Uploaded $source_file to $ftp_server as $destination_file";
+}
+
+// Retrieve directory listing
+$files = ftp_nlist($conn_id, '/remote_dir');
+
+// close the FTP stream 
 ftp_close($conn_id);
+
 
 ?>
